307a167b777a87deab843d94db1b64dac9574f1b
Jamie Schembri <jamie@schembri.me>
2022-11-11 17:01:42 +0100

    Initial

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+INPUT_FILES="$1"
+OUTPUT_FILE="$2"
+
+cyclonedx-cli --input-files "/github/workspace/${INPUT_FILES}" \
+  --output-file "/github/workspace/${OUTPUT_FILE}"
