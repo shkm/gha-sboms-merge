7445102e6e9189488c3e0e5599e583d38eff3f30
Jamie Schembri <jamie@schembri.me>
2022-11-11 17:58:58 +0100

    Relax

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -Eeuo pipefail
 INPUT_FILES=$(ls $1)
 OUTPUT_FILE="$2"
 
-./cyclonedx merge --input-files $INPUT_FILES --output-file "${OUTPUT_FILE}"
+cyclonedx merge --input-files $INPUT_FILES --output-file "${OUTPUT_FILE}"
