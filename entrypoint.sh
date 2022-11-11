6913b62a58f7a3364800824b96281e74153a23ba
Jamie Schembri <jamie@schembri.me>
2022-11-11 17:16:17 +0100

    idk

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -Eeuo pipefail
 INPUT_FILES="$1"
 OUTPUT_FILE="$2"
 
-/cyclonedx --input-files "/github/workspace/${INPUT_FILES}" \
+./cyclonedx --input-files "/github/workspace/${INPUT_FILES}" \
   --output-file "/github/workspace/${OUTPUT_FILE}"
